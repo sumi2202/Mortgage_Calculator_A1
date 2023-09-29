66262b8c137151f6133c97e5c99b55e28b4ffe2f
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-09-29 10:05:37 -0400

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mortgage_Calculator_Assignment1"
+include(":app")
+ 
