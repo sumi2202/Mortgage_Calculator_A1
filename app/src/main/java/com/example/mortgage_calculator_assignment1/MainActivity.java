50f97a2fe0998380481038bc24c700a2d8ed68b5
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-10-01 01:36:31 -0400

    java fun

--- app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
+++ app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
@@ -3,8 +3,21 @@ package com.example.mortgage_calculator_assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText loanAmount;
+    private EditText interest;
+    private EditText year;
+    private EditText month;
+
+    private TextView x_emi;
+    private TextView x_tenure;
+    private TextView x_loanAmount;
+    private TextView x_interestPayable;
+    private TextView x_totalPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
