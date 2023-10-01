fd2ed1423c7fd30287ff5485a6d9fda289865775
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-10-01 02:33:22 -0400

    fixes

--- app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
+++ app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button loanSummaryButton = findViewById(R.id.buttonTotal);
+        Button clearButton = findViewById(R.id.buttonClear);
+
         principalAmount = (EditText)findViewById(R.id.principalAmount);
         interest = (EditText)findViewById(R.id.interest);
         inYear = (EditText)findViewById(R.id.year);
@@ -37,19 +41,32 @@ public class MainActivity extends AppCompatActivity {
         x_loanAmount = (TextView)findViewById(R.id.loanAmount);
         x_interestPayable = (TextView)findViewById(R.id.intPayable);
         x_totalPayment = (TextView)findViewById(R.id.totalPayable);
+
+        loanSummaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double M_principalAmount = Double.parseDouble(principalAmount.getText().toString());
+                double M_interest = Double.parseDouble(interest.getText().toString());
+                int M_year = Integer.parseInt(inYear.getText().toString());
+                int M_month = Integer.parseInt(inMonth.getText().toString());
+
+                int yearExchange = (M_year * 12) + M_month;
+                double principal = M_principalAmount * (M_interest / 100);
+                double power = Math.pow(M_interest / 100 + 1, yearExchange);
+                double sum = principal / (1 - (1 / power));
+
+                double TotalInterest = sum * yearExchange - M_principalAmount;
+                double TotalPayment = M_principalAmount + TotalInterest;
+
+
+                x_emi.setText(String.valueOf(sum));
+                x_tenure.setText(String.valueOf(yearExchange) + "Month");
+                x_loanAmount.setText(String.valueOf(M_principalAmount));
+                x_interestPayable.setText(String.valueOf(TotalInterest));
+                x_totalPayment.setText(String.valueOf(TotalPayment));
+
+            }
+        });
     }
 
-    public void buttonTotal(View view){
-        double M_principalAmount = Integer.parseInt(principalAmount.getText().toString());
-        double M_interest = Integer.parseInt(interest.getText().toString());
-        int M_year = Integer.parseInt(inYear.getText().toString());
-        int M_month = Integer.parseInt(inMonth.getText().toString());
-
-        int yearExchange = (M_year * 12) + M_month;
-        double principal = M_principalAmount * (M_interest / 100);
-        double power = Math.pow(M_interest / 100 + 1, yearExchange);
-        double sum = principal / (1 - (1 / power));
-
-
-    }
 }
