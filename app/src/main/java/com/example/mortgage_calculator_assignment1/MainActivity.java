794acd359b3e501cd70f7c5582be04b45fc40660
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-10-01 02:00:02 -0400

    more java

--- app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
+++ app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView x_emi;
     private TextView x_tenure;
-    private TextView x_principalAmount;
+    private TextView x_loanAmount;
     private TextView x_interestPayable;
     private TextView x_totalPayment;
 
@@ -27,9 +28,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         principalAmount = (EditText)findViewById(R.id.principalAmount);
-        interest = (EditText)findViewById(R.id.inMonth);
+        interest = (EditText)findViewById(R.id.interest);
         inYear = (EditText)findViewById(R.id.year);
         inMonth = (EditText)findViewById(R.id.month);
+
+        x_emi = (TextView)findViewById(R.id.EMI);
+        x_tenure = (TextView)findViewById(R.id.tenure);
+        x_loanAmount = (TextView)findViewById(R.id.loanAmount);
+        x_interestPayable = (TextView)findViewById(R.id.intPayable);
+        x_totalPayment = (TextView)findViewById(R.id.totalPayable);
+    }
+
+    public void buttonTotal(View view){
+        double principalAmount1 = Integer.parseInt(principalAmount.getText().toString());
+        double interest1 = Integer.parseInt(interest.getText().toString());
+        double year1 = Integer.parseInt(inYear.getText().toString());
+        double month1 = Integer.parseInt(inMonth.getText().toString());
+
 
     }
 }
