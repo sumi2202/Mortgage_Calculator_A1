07e2569cec1b871ef26c0759178b8a984a155fd7
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-10-01 01:47:46 -0400

    find by id

--- app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
+++ app/src/main/java/com/example/mortgage_calculator_assignment1/MainActivity.java
@@ -2,26 +2,34 @@ package com.example.mortgage_calculator_assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText loanAmount;
+    private EditText principalAmount;
     private EditText interest;
-    private EditText year;
-    private EditText month;
+    private EditText inYear;
+    private EditText inMonth;
 
     private TextView x_emi;
     private TextView x_tenure;
-    private TextView x_loanAmount;
+    private TextView x_principalAmount;
     private TextView x_interestPayable;
     private TextView x_totalPayment;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        principalAmount = (EditText)findViewById(R.id.principalAmount);
+        interest = (EditText)findViewById(R.id.inMonth);
+        inYear = (EditText)findViewById(R.id.year);
+        inMonth = (EditText)findViewById(R.id.month);
+
     }
 }
