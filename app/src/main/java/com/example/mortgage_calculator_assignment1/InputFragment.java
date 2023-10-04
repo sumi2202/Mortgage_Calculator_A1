92db344a0d96fc4eb5e0b50b6e482ee60087d652
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-10-03 23:27:14 -0400

    imports again

--- app/src/main/java/com/example/mortgage_calculator_assignment1/InputFragment.java
+++ app/src/main/java/com/example/mortgage_calculator_assignment1/InputFragment.java
@@ -1,4 +1,8 @@
 package com.example.mortgage_calculator_assignment1;
-
-public class InputFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+public class InputFragment extends Fragment{
 }
